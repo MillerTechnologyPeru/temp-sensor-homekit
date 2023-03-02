3d63305cd4ab55c48c436fdac4312b648e311c3b
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-01 23:44:42 -0800

    Add `SensorError`

---
+++ Sources/CoreSensor/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 3/1/23.
+//
+
+import Foundation
+
+public enum SensorError: Error {
+    
+    case invalidAdvertisementData
+}
