d9c9a8a64b81139c4ed8a9eb83d9f842f8efbb6b
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-16 00:13:22 -0700

    Add `SensorAccessory`

--- Sources/TempSensorHomeKit/Accessory.swift
+++ Sources/TempSensorHomeKit/Accessory.swift
@@ -1,82 +1,22 @@
 //
-//  Accessory.swift
+//  ThermometerAccessory.swift
 //  
 //
-//  Created by Alsey Coleman Miller on 3/2/23.
+//  Created by Alsey Coleman Miller on 3/15/23.
 //
 
 import Foundation
 import Bluetooth
 import GATT
-import HAP
 import CoreSensor
 
-final class GESensorAccessory: HAP.Accessory.Thermometer {
+protocol SensorAccessory: AnyObject {
     
-    let peripheral: NativeCentral.Peripheral
+    associatedtype Advertisement: SensorAdvertisement
     
-    //let bridgeState = Service.BridgingState()
+    var peripheral: NativeCentral.Peripheral { get }
     
-    let humidity = HAP.Service.HumiditySensor()
+    init(peripheral: NativeCentral.Peripheral, advertisement: Advertisement)
     
-    let battery = BatteryService()
-    
-    init(peripheral: NativeCentral.Peripheral, advertisement: GESensor) {
-        self.peripheral = peripheral
-        let info = Service.Info.Info(
-            name: "GE Sensor",
-            serialNumber: peripheral.description,
-            manufacturer: "GE",
-            model: "\(advertisement.model)",
-            firmwareRevision: "\(advertisement.version)"
-        )
-        super.init(
-            info: info,
-            additionalServices: [
-                //bridgeState,
-                humidity,
-                battery
-            ]
-        )
-        //self.bridgeState.accessoryIdentifier.value = peripheral.description
-        self.update(advertisement: advertisement)
-    }
-    
-    func update(advertisement: GESensor) {
-        self.reachable = true
-        self.battery.batteryVoltage.value = advertisement.batteryVoltage
-        self.battery.batteryLevel?.value = UInt8(advertisement.batteryLevel.rounded())
-        self.battery.statusLowBattery.value = advertisement.batteryLevel < 25 ? .batteryLow : .batteryNormal
-        self.temperatureSensor.currentTemperature.value = advertisement.temperatureCelcius
-        self.humidity.currentRelativeHumidity.value = advertisement.humidityPercentage
-    }
-}
-
-extension GESensorAccessory {
-    
-    final class BatteryService: HAP.Service.Battery {
-        
-        let batteryVoltage = GenericCharacteristic<Float>(
-            type: .custom(UUID(uuidString: "5C7D8287-D288-4F4D-BB4A-161A83A99752")!),
-            value: 3.3,
-            permissions: [.read, .events],
-            description: "Battery voltage",
-            format: .float,
-            unit: .none
-        )
-        
-        init() {
-            let name = PredefinedCharacteristic.name("Battery")
-            let batteryLevel = PredefinedCharacteristic.batteryLevel()
-            let chargingState = PredefinedCharacteristic.chargingState()
-            super.init(characteristics: [
-                AnyCharacteristic(name),
-                AnyCharacteristic(batteryLevel),
-                AnyCharacteristic(chargingState),
-                AnyCharacteristic(batteryVoltage)
-            ])
-            self.statusLowBattery.value = .batteryNormal
-            self.chargingState?.value = .notChargeable
-        }
-    }
+    func update(advertisement: Advertisement)
 }
