d9be06c945d923f26ccab27b33184ce5ac9ce0b5
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-14 03:51:58 -0400

    Updated `TempSensorHomeKitToolError`

--- Sources/TempSensorHomeKit/Error.swift
+++ Sources/TempSensorHomeKit/Error.swift
@@ -5,7 +5,23 @@
 //  Created by Alsey Coleman Miller on 3/1/23.
 //
 
+import Foundation
+import Bluetooth
+
 public enum TempSensorHomeKitToolError: Error {
     
+    /// Bluetooth is not available on this device.
     case bluetoothUnavailable
+    
+    /// No service with UUID found.
+    case serviceNotFound(BluetoothUUID)
+    
+    /// No characteristic with UUID found.
+    case characteristicNotFound(BluetoothUUID)
+    
+    /// The characteristic's value could not be parsed. Invalid data.
+    case invalidCharacteristicValue(BluetoothUUID)
+    
+    /// Not a compatible peripheral
+    case incompatiblePeripheral
 }
